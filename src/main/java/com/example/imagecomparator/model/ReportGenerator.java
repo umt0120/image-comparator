32560ea1082acb5fcca0f410b0ab482452b987d5
UEMOTO Soichiro <uemoto.soichiro@ryobi.co.jp>
2021-07-12 17:41:53 +0900

    fix: modify report html

--- src/main/java/com/example/imagecomparator/model/ReportGenerator.java
+++ src/main/java/com/example/imagecomparator/model/ReportGenerator.java
@@ -1,24 +1,34 @@
 package com.example.imagecomparator.model;
 
 import j2html.tags.DomContent;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.boot.autoconfigure.quartz.QuartzDataSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static j2html.TagCreator.*;
 
 @Component
 public class ReportGenerator {
+
+    private static final String label0Suffix = "_2_0.jpg";
+    private static final String label1Suffix = "_2_1.jpg";
+    private static final String extension = ".jpg";
 
     /**
      * 設定値を記載したYAMLファイルをもとにHTML形式のレポートを出力する
@@ -34,11 +44,15 @@ public class ReportGenerator {
                 .map(rawDirInfo -> {
                     DirectoryInfo dirInfo = new DirectoryInfo();
                     dirInfo.setTitle(rawDirInfo.getTitle());
-                    dirInfo.setFileList(convertToPathList(rawDirInfo.getDir()));
+                    dirInfo.setImgFileNamePairList(convertToFileNameListToImgFileNamePairList(rawDirInfo.getDir()));
+                    // dirInfo.setFileList(convertToPathList(rawDirInfo.getDir()));
+                    // dirInfo.setFileNamesCorrespondingToTheOrigin(convertToFileNameListCorrespondingToTheOrigin(rawDirInfo.getDir()));
                     return dirInfo;
                 }).collect(Collectors.toList());
 
-        String report = renderReport(srcFileList, comparingDirectoryInfos);
+        List<ReportRawInfo> reportRawInfoList = createRawInfo(configYaml, srcFileList, comparingDirectoryInfos);
+
+        String report = renderReport(comparingDirectoryInfos,reportRawInfoList);
         outputReport(report);
     }
 
@@ -54,8 +68,53 @@ public class ReportGenerator {
         return ret;
     }
 
-    private String renderReport(List<Path> srcFileList, List<DirectoryInfo> comparingDirectoryInfos){
-        List<String> labels = Arrays.asList("0", "1");
+    private List<String> convertToFileNameListCorrespondingToTheOrigin(String path){
+        List<String> ret = new ArrayList<>();
+
+        try {
+            ret = Files.walk(Path.of(path))
+                    .filter(eachFile -> !Files.isDirectory(eachFile))
+                    .map(eachFile -> {
+                        // prefix除去（YAMLに書いある./srcとか）
+                        String prefixRemoved = eachFile.toString().replace(path, "");
+                        if(prefixRemoved.contains(label0Suffix)){
+                            // gradcam出力画像に付与されている_2_0とかを取り除いて返す
+                           return prefixRemoved.replace(label0Suffix,extension);
+                        }else{
+                            return prefixRemoved.replace(label1Suffix,extension);
+                        }
+                    })
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+    private List<ImgFileNamePair> convertToFileNameListToImgFileNamePairList(String path){
+        List<ImgFileNamePair> ret = new ArrayList<>();
+
+        try {
+            ret = Files.walk(Path.of(path))
+                    .filter(eachFile -> !Files.isDirectory(eachFile))
+                    .map(eachFile -> {
+                        // prefix除去（YAMLに書いある./srcとか）
+                        String prefixRemoved = eachFile.toString().replace(Paths.get(path).toString(), "");
+                        if(prefixRemoved.contains(label0Suffix)){
+                            // gradcam出力画像に付与されている_2_0とかを取り除いて返す
+                            return new ImgFileNamePair(eachFile,prefixRemoved.replace(label0Suffix,extension));
+                        }else{
+                            return new ImgFileNamePair(eachFile,prefixRemoved.replace(label1Suffix,extension));
+                        }
+                    })
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
+    private String renderReport(List<DirectoryInfo> comparingDirectoryInfos, List<ReportRawInfo> reportRawInfoList){
+
         Stream<DomContent> labelList = comparingDirectoryInfos.stream().flatMap(info -> Stream.of(th("0"), th("1")));
         return table(
                 tbody(
@@ -68,9 +127,44 @@ public class ReportGenerator {
                                 th(),
                                 th(),
                                 each(labelList)
-                        )
+                        ),
+                        each(reportRawInfoList, raw -> tr(
+                                th(raw.getSrcImgPath()),
+                                th(img().withSrc(raw.getSrcImgPath()).withWidth("255").withHeight("255")),
+                                each(raw.getImgPathListToBeCompared(), col -> th(img().withSrc(col.toString()).withWidth("255").withHeight("255")))
+                        ))
                 )
         ).render();
+    }
+
+    private List<ReportRawInfo> createRawInfo(ConfigYaml configYaml, List<Path> srcFileList, List<DirectoryInfo> comparingDirectoryInfos){
+
+        List<ReportRawInfo> reportRawInfoList = new ArrayList<>();
+
+        srcFileList.stream().forEach(srcFile -> {
+
+            ReportRawInfo reportRawInfo = new ReportRawInfo();
+            reportRawInfo.setSrcImgPath(srcFile.toString());
+            List<Path> imgPathListToBeCompared = new ArrayList<>();
+            // 症例以降だけを切り出した元画像ファイルパス
+            String originalFilePathAfterTheCase = srcFile.toString().replace(Paths.get(configYaml.getSrcDir()).toString(), "");
+
+            comparingDirectoryInfos.stream()
+                    .forEach(directoryInfo -> {
+                       List<ImgFileNamePair> pairList =  directoryInfo.getImgFileNamePairList().stream()
+                               .filter(eachFile -> Objects.equals(originalFilePathAfterTheCase.replace(".jpg", "").replace(".bmp",""), eachFile.getFileNameCorrespondingToTheOrigin().replace(".jpg", "").replace(".bmp","")))
+                               .collect(Collectors.toList());
+                       if(pairList.size() == 2){
+                           pairList.stream().forEach(eachFile -> imgPathListToBeCompared.add(eachFile.getRealFilePath()));
+                       }else{
+                           imgPathListToBeCompared.add(Paths.get(""));
+                           imgPathListToBeCompared.add(Paths.get(""));
+                       }
+                    });
+            reportRawInfo.setImgPathListToBeCompared(imgPathListToBeCompared);
+            reportRawInfoList.add(reportRawInfo);
+        });
+        return reportRawInfoList;
     }
 
     private void outputReport(String report) {
@@ -85,8 +179,22 @@ public class ReportGenerator {
     @Data
     private static class DirectoryInfo{
         private String title;
-        private List<Path> fileList;
+        private List<ImgFileNamePair> imgFileNamePairList;
     }
 
+    @AllArgsConstructor
+    @Data
+    private static class ImgFileNamePair{
+       private Path realFilePath;
+       private String fileNameCorrespondingToTheOrigin;
+    }
+
+    @Data
+    private static class ReportRawInfo{
+        /** 比較元画像ファイルパス*/
+        private String srcImgPath;
+        /** 比較対象画像ファイルパスリスト（GradCAM画像（ラベル0）,GradCAM画像（ラベル1）, GradCAM++画像（ラベル0）, GradCAM++画像（ラベル1） ....）*/
+        private List<Path> imgPathListToBeCompared;
+    }
 
 }
